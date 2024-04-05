4ffd9b205c629dc8548a2ffbc444035bdf9efe92
CuiHen <104631322+CuiHen@users.noreply.github.com>
2024-04-05 10:33:21 +0200

    Update compile.sh

--- cgp_boolean_crossover/compile.sh
+++ cgp_boolean_crossover/compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cargo build --features "tournament standard" --release --target-dir tournament_vanilla
+cargo build --features "tournament standard" --release --target-dir tournament_standard
 cargo build --features "tournament ereorder" --release --target-dir tournament_ereorder
-cargo build --features "mulambda_crossover standard" --release --target-dir mulambda_vanilla
+cargo build --features "mulambda_crossover standard" --release --target-dir mulambda_standard
 cargo build --features "mulambda_crossover ereorder" --release --target-dir mulambda_ereorder
 cargo build --features "mulambda standard" --release --target-dir standard
 cargo build --features "mulambda ereorder" --release --target-dir standard_ereorder
